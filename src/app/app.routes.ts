3c685dea9d53a7fae8488901bc349fa607f030cb
ozkurkculer <mehmetemin.ozkurkculer@gmail.com>
2024-07-10 17:54:22 +0300

    feat: Add new routes and Http provider

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    title: 'Home',
+    component: HomeComponent,
+  },
+  {
+    path: 'about',
+    title: 'About',
+    component: AboutComponent,
+  },
+];
