44829d13ca9209552fc77763f4e0ab7fa0a0d85c
ozkurkculer <mehmetemin.ozkurkculer@gmail.com>
2024-07-10 21:48:43 +0300

    feat: Refactor NewsService to use dynamic URLs for different API requests

--- src/app/services/news.service.ts
+++ src/app/services/news.service.ts
@@ -10,7 +10,6 @@ import { map, Observable } from 'rxjs';
 export class NewsService {
   private apiUrl = 'https://newsapi.org/v2/';
   private http = inject(HttpClient);
-  private allNewsUrl = `${this.apiUrl}top-headlines?country=tr&pageSize=100&apiKey=${environment.apiKey}`;
 
   private options = {
     method: 'GET',
@@ -20,6 +19,17 @@ export class NewsService {
   };
 
   getAllNews(): Observable<News> {
-    return this.http.get<News>(this.allNewsUrl, this.options);
+    const allNewsUrl = `${this.apiUrl}top-headlines?country=tr&pageSize=100&apiKey=${environment.apiKey}`;
+    return this.http.get<News>(allNewsUrl, this.options);
+  }
+
+  getNewsByCategory(category: string): Observable<News> {
+    const url = `${this.apiUrl}top-headlines?country=tr&category=${category}&pageSize=100&apiKey=${environment.apiKey}`;
+    return this.http.get<News>(url, this.options);
+  }
+
+  getNewsByQuery(query: string): Observable<News> {
+    const url = `${this.apiUrl}top-headlines?country=tr&q=${query}&pageSize=100&apiKey=${environment.apiKey}`;
+    return this.http.get<News>(url, this.options);
   }
 }
