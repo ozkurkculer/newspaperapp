48d5cd675898ca7c44751917ad803d876b4379ac
ozkurkculer <mehmetemin.ozkurkculer@gmail.com>
2024-07-10 21:48:08 +0300

    feat: Add category and search page routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
-import { Routes } from '@angular/router';
+import { Routes, ActivatedRouteSnapshot, Router } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { CategoryPageComponent } from './category-page/category-page.component';
+import { SearchPageComponent } from './search-page/search-page.component';
 
 export const routes: Routes = [
   {
@@ -13,4 +15,19 @@ export const routes: Routes = [
     title: 'About',
     component: AboutComponent,
   },
+  {
+    path: 'category/:category',
+    component: CategoryPageComponent,
+    title: (route: ActivatedRouteSnapshot): string => {
+      const category = route.paramMap.get('category');
+      return `${category?.charAt(0).toUpperCase()}${category?.slice(1)}`;
+    },
+  },
+  {
+    path: 'search/:query',
+    component: SearchPageComponent,
+    title: (route: ActivatedRouteSnapshot): string => {
+      return `Results - "${route.paramMap.get('query')}"`;
+    },
+  }
 ];
