541c235d18d2fdc9d54b34a83dcac9ee86cd75cd
ozkurkculer <mehmetemin.ozkurkculer@gmail.com>
2024-07-10 17:54:58 +0300

    feat: Add News interface and related types

---
+++ src/app/news.ts
@@ -0,0 +1,21 @@
+export interface News {
+  status: string;
+  totalResults: number;
+  articles: Article[];
+}
+
+export interface Article {
+  source: Source;
+  author: string;
+  title: string;
+  description: string | null;
+  url: string;
+  urlToImage: string | null;
+  publishedAt: string;
+  content: string | null;
+}
+
+interface Source {
+  id: string;
+  name: string;
+}
